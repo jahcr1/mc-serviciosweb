bb212e422d2855d5af16b0814772d71569233042
jahcr1 <martineta_contreras@hotmail.com>
2025-08-22 16:38:01 -0300

    primeras pruebas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,6 @@
 <div class="text-center py-5">
   <h1>Bienvenido a {{ config('app.name') }}</h1>
   <p>Desarrollo web profesional para hacer crecer tu negocio</p>
-  <a href="{{ route('contact') }}" class="btn btn-primary">Solicita tu presupuesto</a>
+  <a href="{{ route('contact') }}" class="btn btn-danger">Solicita tu presupuesto</a>
 </div>
 @endsection
